472bcf404a58e8bc93c689f2662d6be3f2c73e18
KitKatKittieKat <m_d_clarke@hotmail.com>
2020-06-24 23:05:18 +0900

    started removing inline code where possible for index and home pages

---
+++ dist/js/lazy.js
@@ -0,0 +1,12 @@
+if ('loading' in HTMLImageElement.prototype) {
+    // Native loading supported
+    const images = document.querySelectorAll('img[loading="lazy"]');
+    images.forEach(img => {
+        img.src = img.dataset.src;
+    });
+} else {
+    // dynamically import the LazySizes lib if native loading not supported
+    const script = document.createElement('script');
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+    document.body.appendChild(script);
+}
