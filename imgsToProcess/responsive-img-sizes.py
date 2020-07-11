f69975f4619f4e37b1c92f43dd46d7a8b268b69d
KitKatKittieKat <m_d_clarke@hotmail.com>
2020-07-11 14:35:02 +0900

    added python script to make json output for responsive image sizes

---
+++ imgsToProcess/responsive-img-sizes.py
@@ -0,0 +1,104 @@
+# create .json output for responsive images (widths and heights) (responsive images made using gulp)
+# need to CD into imgsToProcess directory
+import os
+from PIL import Image
+
+
+
+# Loop over all files in the working directory.
+imgWidthsModal = []
+for filename in os.listdir('.'): # current directory
+    if not (filename.endswith('.jpg')):
+        continue # skip python file
+
+    im = Image.open(filename)
+    w, h = im.size  # tuple
+
+    # image sizes used depends on the max with of the original image
+    if (w >= 1024 ):
+        imageSizes = [375, 563, 650, 800, 1024]
+    elif ( 800 < w < 1024 ):
+        imageSizes = [375, 563, 650, 800]
+        imageSizes.append(w)
+    elif ( w == 800 ):
+        imageSizes = [375, 563, 650, 800]
+    elif ( 650 < w < 800 ):
+        imageSizes = [375, 563, 650]
+        imageSizes.append(w)
+    elif ( w == 650 ):
+        imageSizes = [375, 563, 650]
+    elif ( 563 < w < 650 ):
+        imageSizes = [375, 563]
+        imageSizes.append(w)
+
+    imgWidthsModal.append(imageSizes)
+
+imgWidthsModalOutput = '"imgWidthsModal": \n' + str(imgWidthsModal) + '\n'
+
+
+
+imageHeightsModalMax = []
+for filename in os.listdir('.'):
+    if not (filename.endswith('.jpg')):
+        continue # skip python file
+
+    im = Image.open(filename)
+    w, h = im.size
+    maxHeight =[]
+    maxHeight.append(h)
+    imageHeightsModalMax.append(maxHeight)
+
+imageHeightsModalMaxOutput = '"imageHeightsModalMax": \n' + str(imageHeightsModalMax) + '\n'
+
+
+
+# Loop over all files in the working directory.
+imgWidthsNonModal = []
+for filename in os.listdir('.'):
+    if not (filename.endswith('.jpg')):
+        continue # skip python file
+
+    im = Image.open(filename)
+    w, h = im.size  
+
+    if (w >= 650 ):
+        imageSizes = [375, 563, 650]
+    elif ( 563 < w < 650 ):
+        imageSizes = [375, 563]
+        imageSizes.append(w)
+    elif ( w == 563 ):
+        imageSizes = [375, 563]
+    elif ( 375 < w < 563 ):
+        imageSizes = [375]
+        imageSizes.append(w)
+
+    imgWidthsNonModal.append(imageSizes)
+
+imgWidthsNonModalOutput = '"imgWidthsNonModal": \n' + str(imgWidthsNonModal) + '\n'
+
+
+
+imgHeightsNonModalMax = []
+for filename in os.listdir('.'):
+    if not (filename.endswith('.jpg')):
+        continue # skip python file
+
+    im = Image.open(filename)
+    w, h = im.size
+    maxHeight =[]
+    if (w >= 650 ):
+        # calculate max height of image that is 650px wide
+        heightCalculated = int(650 / w * h)
+        maxHeight.append(heightCalculated)
+    elif ( w < 650 ):
+        maxHeight.append(h)
+
+    imgHeightsNonModalMax.append(maxHeight)
+
+imgHeightsNonModalMaxOutput = '"imgHeightsNonModalMax": \n' + str(imgHeightsNonModalMax) + '\n'
+
+
+
+# join all the img size arrays together
+responsiveImgSizesAll = imgWidthsModalOutput + ',' + imageHeightsModalMaxOutput + ',' +  imgWidthsNonModalOutput + ',' +  imgHeightsNonModalMaxOutput
+print(responsiveImgSizesAll)
