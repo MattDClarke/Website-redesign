dd6d0d39e815e31bf54e4e16f2aa51a4cfbae0dd
KitKatKittieKat <m_d_clarke@hotmail.com>
2020-12-27 12:21:28 +0900

    Added meta descriptions and fixed Google Analytics Cookie issue in Firefox - set 'secure' attribute

--- js/googleAnalytics.js
+++ js/googleAnalytics.js
@@ -4,4 +4,7 @@ function gtag() {
 }
 gtag('js', new Date());
 
-gtag('config', 'UA-169851286-1');
+gtag('config', 'UA-169851286-1', {
+  cookie_domain: 'snakes-of-taiwan-test.netlify.app',
+  cookie_flags: 'SameSite=None;Secure',
+});
